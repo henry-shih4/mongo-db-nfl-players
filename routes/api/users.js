6366e6bef17959460e97823717cccc17fd5b8d5a
henry-shih4 <100496651+henry-shih4@users.noreply.github.com>
2022-10-31 11:58:58 -0500

    login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt')
-const users = [];
+const bcrypt = require("bcrypt");
+const users = [
+  {
+    username: "ssong",
+    password: "password",
+  },
+];
 
 // Load model
 const User = require("../../models/User");
@@ -9,39 +14,50 @@ const User = require("../../models/User");
 // @route GET api/users/test
 router.get("/test", (req, res) => res.send("user test!"));
 
-
-
 // @route get api/users/
 router.get("/", (req, res) => {
-    User.find()
-      .then((user) => res.json(user))
-      .catch((err) =>
-        res.status(404).json({ nousersfound: "No users found" })
-      );
-  });
+  User.find()
+    .then((user) => res.json(user))
+    .catch((err) => res.status(404).json({ nousersfound: "No users found" }));
+});
 
-
-  // @route POST to api/users
+// @route POST to api/users
+// add a new user to DB
 router.post("/", async (req, res) => {
-    try{
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(req.body.password, salt)
-        console.log(salt)
-        console.log(hashedPassword)
-        const user = {username:req.body.username, password: hashedPassword, email:req.body.email}
-        User.create(user)
-        res.status(201).send()
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const user = {
+      username: req.body.username,
+      password: hashedPassword,
+      email: req.body.email,
+    };
+    User.create(user);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+  // User.create(req.body)
+  //   .then(() => res.json({ msg: "User added successfully" }))
+  //   .catch((err) =>
+  //     res.status(400).json({ error: "Unable to add user" })
+  //   );
+});
+
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
+
+  if (user === null) {
+    return res.status(400).send();
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("success");
+    } else {
+      res.send("not allowed");
     }
-    catch{
-        res.status(500).send()
-    }
-    // User.create(req.body)
-    //   .then(() => res.json({ msg: "User added successfully" }))
-    //   .catch((err) =>
-    //     res.status(400).json({ error: "Unable to add user" })
-    //   );
-  });
-
-
-
+  } catch {
+    res.status(500).send();
+  }
+});
 module.exports = router;
